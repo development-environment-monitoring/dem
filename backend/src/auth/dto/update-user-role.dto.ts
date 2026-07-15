123565b0d61466fdd2c0c2871b1d2a6dcdd85a85
kielsonzinn <kielsonzinn@gmail.com>
2026-07-15 14:05:37 -0300

    move frontend and backend to other repositories

--- backend/src/auth/dto/update-user-role.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsIn, IsString } from 'class-validator';
-
-export class UpdateUserRoleDto {
-  @IsString()
-  @IsIn(['ADMIN', 'NORMAL'])
-  role: 'ADMIN' | 'NORMAL';
-}
--- backend/src/verification-results/dto/update-machine-alias.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, MaxLength } from 'class-validator';
-
-export class UpdateMachineAliasDto {
-  @IsString()
-  @MaxLength(120)
-  alias: string;
-}
